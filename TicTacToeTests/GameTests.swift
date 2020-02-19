a11223838894c728210511115e6cb6b92bb5fabd
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-19 15:17:18 -0800

    finished tests for restart method

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -11,12 +11,48 @@ import XCTest
 
 class GameTests: XCTestCase {
     
+    //=======================
+    // MARK: - Test isEmpty implementation
+    func testBoardIsEmptyAtStart() {
+        let board = GameBoard()
+        XCTAssertTrue(board.isEmpty == true)
+    }
+    
+    func testBoardIsNotEmptyAfterMove() {
+        var board = GameBoard()
+        try! board.place(mark: .o, on: (0,0))
+        XCTAssertTrue(board.isEmpty == false)
+    }
+    
+    //=======================
+    // MARK: - test game state after restart
     func testBoardIsEmptyAfterRestart() {
+        var board = GameBoard()
+        try! board.place(mark: .x, on: (0,0))
+        var game = Game(board: board, activePlayer: .o, gameIsOver: false)
+        game.restart()
+        XCTAssertTrue(game.board.isEmpty == true)
+    }
+    
+    func testActivePlayerAfterRestart() {
+        let board = GameBoard()
+        var game = Game(board: board, activePlayer: .o, gameIsOver: false)
+        game.restart()
+        XCTAssertTrue(game.activePlayer == .x)
+    }
+    
+    func testGameNotOverAfterRestart() {
         let board = GameBoard()
         var game = Game(board: board, activePlayer: .o, gameIsOver: true)
         game.restart()
-        XCTAssertTrue(game.activePlayer == .x)
         XCTAssertTrue(game.gameIsOver == false)
+    }
+    
+    func testNoWinningPlayerAfterRestart() {
+        let board = GameBoard()
+        var game = Game(board: board, activePlayer: .o, gameIsOver: true, winningPlayer: .o)
+        game.restart()
+        XCTAssertTrue(game.winningPlayer == nil)
     }
     
 }
